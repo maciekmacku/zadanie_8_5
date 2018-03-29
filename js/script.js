0d0813d2d193dd1c05ec245949b2099a45626cf6
maciekmacku <maciejszalaj@gmail.com>
2018-03-29 22:45:20 +0200

    triangle Area finished

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// zadanie 8.5 z polem trojkata
+var a = prompt('first value');
+var h = prompt('second value');
+var triangleArea = a*h/2;
+alert('Your triangle area is:' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
